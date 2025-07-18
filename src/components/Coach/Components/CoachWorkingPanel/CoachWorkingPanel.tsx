d35be7810e3231eb9f208b92e8d04a72f041971e
ivankhoda01 <ivankhoda01@gmail.com>
2025-07-18 14:37:20 +0300

    FIT-108. Prepare for stores review.

--- src/components/Coach/Components/CoachWorkingPanel/CoachWorkingPanel.tsx
+++ src/components/Coach/Components/CoachWorkingPanel/CoachWorkingPanel.tsx
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import './CoachWorkingPanel.style.scss';
 import SmallControlPanel from '../../../Common/SmallControlPanel/SmallControlPanel';
+import { isIOS } from '../../../../utils/isIos';
+import { useCapacitorKeyboardAvoiding } from '../../../Common/useCapacitorKeyboardAvoiding';
+import { useLocation } from 'react-router';
 
 interface CoachPanelProps {
     children?: React.ReactNode;
 }
 
-const CoachPanel: React.FC<CoachPanelProps> = ({ children }) => (
-    <div className={'admin-grids' }>
+const CoachPanel: React.FC<CoachPanelProps> = ({ children }) => {
+    const location = useLocation();
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    if (isIOS) {
+        useCapacitorKeyboardAvoiding(containerRef);
+    }
+
+    if (isIOS) {
+        useCapacitorKeyboardAvoiding(containerRef);
+    }
+
+    return (<div className={'admin-grids'} ref={containerRef}>
 
         <main className="admin-main">
             {location.pathname !== '/' && <SmallControlPanel/>}
             {children}
         </main>
-    </div>
-);
+    </div>);
+};
 
 export default CoachPanel;
