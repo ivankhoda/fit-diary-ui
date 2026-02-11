f3367fe1ee3541889913bdfdcee6dc5af4287517
ivankhoda <ivankhoda01@gmail.com>
2026-02-11 09:10:17 +0300

    FIT-0. Remove api calls

--- src/utils/apiUrl.ts
+++ src/utils/apiUrl.ts
@@ -4,10 +4,10 @@ const getApiBaseUrl = (): string => {
     const baseUrl = `${window.location.protocol}//${window.location.hostname}`;
 
     if (window.location.protocol === 'capacitor:' || Capacitor.isNativePlatform()) {
-        return 'https://planka.tech/api';
+        return 'https://planka.tech/';
     }
 
-    const url = process.env.NODE_ENV === 'production' ? `${baseUrl}/api` : `${baseUrl}:3000`;
+    const url = process.env.NODE_ENV === 'production' ? `${baseUrl}` : `${baseUrl}:3000`;
 
     return url;
 };
